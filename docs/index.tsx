d7a3fdf9c5918e834189f8d528bd9042a52017c5
tony <antbradley91@gmail.com>
2021-06-26 14:10:16 -0400

    next version

--- docs/index.tsx
+++ docs/index.tsx
@@ -31,10 +31,10 @@ function main (sources: Sources): {
       return (
         <div key='app' className='max-width-3 mx-auto p2'>
           <div className='mb3 pb2 border-bottom pure-form pure-form-stacked max-width-1 mx-auto'>
-            <label for='search-input'>Enter property name</label>
-            <input id='search-input' className='col-12' placeholder='Example: "margin" or "padding-right"' />
+            <label for='search-input'>Enter property name or variable</label>
+            <input id='search-input' className='col-12' placeholder='Example: "margin" or "--space"' />
           </div>
-          {filterVal !== '_'
+          {filterVal !== ''
             ? Object.keys(properties)
               .filter(function (propertyName) {
                 return propertyName.toUpperCase().includes(filter)
@@ -46,46 +46,43 @@ function main (sources: Sources): {
               })
               .map(function (propertyName) {
                 const pairs = properties[propertyName]
+                const classes = pairs.map(function ([className, propertyValue]) {
+                  const siblings = selectors[className]
+                  const siblingProperties = siblings.length > 1
+                    ? siblings.filter(([sibPropertyName]) => sibPropertyName.indexOf('--') !== 0)
+                      .filter(function ([sibPropertyName, sibValue]) {
+                        return sibPropertyName !== propertyName || sibValue !== propertyValue
+                      })
 
+                    : []
+                  return {
+                    className,
+                    propertyValue,
+                    siblingProperties
+                  }
+                })
                 return (
-                  <div key={propertyName} className='flex pb2 mb2 border-bottom'>
-                    <span className='col-4 md-col-6'>
-                      {propertyName}
-                    </span>
-                    <div className='col-8 md-col-6'>
-                      {pairs.map(function ([className, value]) {
-                        const siblings = selectors[className]
-                        const otherSiblings = siblings.length > 1
-                          ? siblings.filter(([sibPropertyName]) => sibPropertyName.indexOf('--') !== 0)
-                            .filter(function ([sibPropertyName, sibValue]) {
-                              return sibPropertyName !== propertyName || sibValue !== value
-                            })
-                          : []
-
-                        return (
-                          <div key={className} className='mb1'>
-                            <span className='bold pr1' data-id={className}>{className}</span><i>{value}</i>
-                            {(otherSiblings.length > 0)
-                              ? (
-                                <div className='pl2 h5 mb2 mt1'>
-                                  {otherSiblings.map(function ([sibPropertyName, sibValue]) {
-                                    return (
-                                      <div key={sibPropertyName}>
-                                        <span className='pr1' data-id={className}>{sibPropertyName}:</span><i>{sibValue}</i>
-                                      </div>
-                                    )
-                                  })}
-                                </div>
-                              )
-                              : null}
-                          </div>
-                        )
-                      })}
-                    </div>
-                  </div>
+                  <PropertyDisplay
+                    key={propertyName}
+                    propertyName={propertyName}
+                    classes={classes}
+                  />
                 )
               })
-            : null}
+            : (
+              <PropertyDisplay
+                propertyName='property-name'
+                classes={[
+                  {
+                    propertyValue: 'property-value',
+                    className: '.class-name',
+                    siblingProperties: [
+                      ['other-property', 'property-value']
+                    ]
+                  }
+                ]}
+              />
+            )}
         </div>
       )
     })
@@ -93,6 +90,48 @@ function main (sources: Sources): {
   return {
     DOM: view$
   }
+}
+
+interface PropertyDisplayProps {
+  key?: string
+  propertyName: string
+  classes: Array<{
+    propertyValue: string
+    className: string
+    siblingProperties: Array<[string, string]>
+  }>
+}
+
+function PropertyDisplay (props: PropertyDisplayProps): JSX.Element {
+  return (
+    <div className='flex pb2 mb2 border-bottom'>
+      <span className='col-4 md-col-6'>
+        {props.propertyName}
+      </span>
+      <div className='col-8 md-col-6'>
+        {props.classes.map(function ({ propertyValue, className, siblingProperties }) {
+          return (
+            <div className='mb1' key={className}>
+              <span className='bold pr1'>{className}</span><i>{propertyValue}</i>
+              {siblingProperties.length > 0
+                ? (
+                  <div className='pl3 h5 mb2 mt1'>
+                    {siblingProperties.map(function ([siblingPropertyName, siblingPropertyValue]) {
+                      return (
+                        <div key={siblingPropertyName}>
+                          <span className='pr1'>{siblingPropertyName}:</span><i>{siblingPropertyValue}</i>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )
+                : null}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 if (process.env.NODE_ENV !== 'test') {
